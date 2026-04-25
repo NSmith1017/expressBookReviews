791356cfdffbc1b209f559cd3c86caf04f205002
Noah Smith <nsmith47@ycp.edu>
2026-04-25 01:54:42 -0400

    Update authentication mechanism in index.js
    Refactor authentication mechanism to use 'passToken' instead of 'accessToken' and improve error handling.

--- final_project/index.js
+++ final_project/index.js
@@ -10,26 +10,34 @@ app.use(express.json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
+app.use("/books", books_route);
+
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
-if(req.session.authorization) {
-    let token = req.session.authorization['accessToken']; // Access Token
-    jwt.verify(token, "access",(err,user)=>{
-        if(!err){
-            req.user = user;
-            next();
+
+    console.log(req.session)
+    {
+        if(req.session.authorization)
+        {
+            let token = req.session.authorization['passToken'];
+
+            jwt.verify(token, "access", (err, user) => {
+                if(!err){
+                    req.user = user;
+                    next();
+                }
+                else{
+                    return res.status(403).json({message: "Authentication failed"})
+                }
+            });
         }
-        else{
-            return res.status(403).json({message: "User not authenticated"})
+        else {
+            return res.status(403).json({message: "User not logged into system"})
         }
-     });
- } else {
-     return res.status(403).json({message: "User not logged in"})
- }
-});
+    });
  
 const PORT =5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
+
 app.listen(PORT,()=>console.log("Server is running"));
